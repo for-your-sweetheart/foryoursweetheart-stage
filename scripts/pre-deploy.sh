593f2b215854b72c7c139bebd17c3c823ce7712e
kkesuma <>
2019-06-27 14:15:24 +0800

    add: pre-deployment shell script. update travis task

---
+++ scripts/pre-deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+  echo "invalid branch"
+else
+  echo "BRANCH: $1"
+
+  echo "copying build files"
+  mkdir -p ./public
+  rm -rf ./public/*
+
+  cp -vr ./wp-content/ ./public/
+
+  # remove index.html
+  # we don't want to copy the following 3 files
+  # TODO: maybe use find command and exclude these 3 files, to make it cleaner
+  rm -rf ./public/index.html
+  rm -rf ./public/404.html
+  rm -rf ./public/robots.txt
+
+  cp -vr ./root-content/ ./public/
+fi
