7cc79d77dcc30739ed57ecc34b573f2dd8ddf41a
kkesuma <>
2019-06-27 16:06:57 +0800

    fix: change pre-deploy script, un-necessary branch name check

--- scripts/pre-deploy.sh
+++ scripts/pre-deploy.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
+echo "copying build files"
+mkdir -p ./public
+rm -rf ./public/*
 
-if [ -z "$1" ]
-then
-  echo "invalid branch"
-else
-  echo "BRANCH: $1"
+cp -vr ./wp-site-content/ ./public/
 
-  echo "copying build files"
-  mkdir -p ./public
-  rm -rf ./public/*
+# remove index.html
+# we don't want to copy the following 3 files
+# TODO: maybe use find command and exclude these 3 files, to make it cleaner
+rm -rf ./public/index.html
+rm -rf ./public/404.html
+rm -rf ./public/robots.txt
 
-  cp -vr ./wp-site-content/ ./public/
-
-  # remove index.html
-  # we don't want to copy the following 3 files
-  # TODO: maybe use find command and exclude these 3 files, to make it cleaner
-  rm -rf ./public/index.html
-  rm -rf ./public/404.html
-  rm -rf ./public/robots.txt
-
-  cp -vr ./root-content/ ./public/
-fi
+cp -vr ./root-content/ ./public/
