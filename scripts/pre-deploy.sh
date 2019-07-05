d91878bcb2e2296aea8cd29f55c3c732fecb1cdf
Kencana <kencana@C02XKBGLJG5H.local>
2019-07-05 13:55:13 +0800

    fix: change firebase build folder name from public to firebase-dist

--- scripts/pre-deploy.sh
+++ scripts/pre-deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 echo "copying build files"
-mkdir -p ./public
-rm -rf ./public/*
+mkdir -p ./firebase-dist
+rm -rf ./firebase-dist/*
 
-cp -vr ./wp-site-content/* ./public/
+cp -vr ./wp-site-content/* ./firebase-dist/
 
 # remove index.html
 # we don't want to copy the following 3 files
 # TODO: maybe use find command and exclude these 3 files, to make it cleaner
-rm -rf ./public/index.html
-rm -rf ./public/404.html
-rm -rf ./public/robots.txt
+rm -rf ./firebase-dist/index.html
+rm -rf ./firebase-dist/404.html
+rm -rf ./firebase-dist/robots.txt
 
-cp -vr ./root-content/* ./public/
+cp -vr ./root-content/* ./firebase-dist/
